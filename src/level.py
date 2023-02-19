4194041dc8987b98b2e39f43b5d7a770315778a1
BryanCastro <bryanc77777@gmail.com>
2023-02-18 23:31:44 -0800

    Camera & YSort
    -Added Custom YSortCamera
    -Render Sprites by Y Position
    -Player Centered on Screen and World Camera renders rest of world based on an offset

--- src/level.py
+++ src/level.py
@@ -10,7 +10,7 @@ class Level:
         self.display_surface = pygame.display.get_surface()
         
         #sprite group setup
-        self.visible_sprites = pygame.sprite.Group()
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
         #sprite setup
@@ -29,6 +29,29 @@ class Level:
 
     def run(self):
         #update game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+
+        #general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0]//2
+        self.half_height = self.display_surface.get_size()[1]//2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        #getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        #debug(player.rect.center)
+        #debug([self.half_width, self.half_height],30)
+        #debug(self.offset, 50)
+
+        #sort and render sprites by Y Position
+        for sprite in sorted(self.sprites(), key= lambda sprite: sprite.rect.centery):
+            offset_rect = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_rect)
